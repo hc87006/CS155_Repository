3becb0b8c1d6fa04c0fb78cc3605a6faff2994f5
Haydn Christian <hc87006@outlook.com>
2024-08-28 20:39:30 -0700

    Add project files.

---
+++ Homework1Lab1/Homework1Lab1.cs
@@ -0,0 +1,34 @@
+﻿/// Homework No.1 Exercise No.1
+/// File Name: Homework1Lab1.cs
+/// @author: Haydn Christian
+/// Date: Aug. 21, 2024
+///
+/// Problem Statement: (Display a shape)
+/// 
+/// Overall Plan (step-by-step, how you want the code to make it happen):
+/// 1. Use WriteLine statements to make a square
+///
+/// Classes needed and Purpose (DrawSquare to draw the square)
+/// main class - Homework1Lab1
+/// Import necessary C# or user-defined packages
+/// Create main class and objects needed to implement tasks
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1Lab1
+{
+    internal class DrawSquare
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ----");
+            Console.WriteLine("|    |");
+            Console.WriteLine("|    |");
+            Console.WriteLine(" ----");
+
+        }
+    }
+}
