8187ce04871347a619cce3841e4c8cec49e9dc87
Haydn Christian <hc87006@outlook.com>
2024-09-13 19:56:16 -0700

    Homework3Lab1 upoad

---
+++ Homework3_Lab1/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OutstandingMortgage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BalanceTextBox_KeyDown(Object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (BalanceTextBox.Text.All(char.IsDigit))
+                {
+                    const double interest = 6.39;
+                    double balance = double.Parse(BalanceTextBox.Text);
+                    double payment = double.Parse(PaymentTextBox.Text);
+
+                    double interestPaid = Math.Round((balance * interest) / 12);
+
+                    double principal = Math.Round((payment - interestPaid), 2);
+
+                    double balanceRemaining = Math.Round((balance - principal), 2);
+
+                    NewBalance.Text = balanceRemaining.ToString();
+                    Principal.Text = principal.ToString();
+                    InterestPaid.Text = interestPaid.ToString();
+                    
+                }
+
+                
+            }
+        }
+
+    }
+}
