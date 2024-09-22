c02e4c4242f2401b65d20b45cf065f8cd03ae7e1
Haydn Christian <hc87006@outlook.com>
2024-09-21 17:01:37 -0700

    create Homework4_Part1 and  Homework4_Part2

---
+++ Homework4_Part1/BMI_Calculator.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System.Configuration;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Homework4_Part1
+{
+    public partial class BMI_Calculator : Window
+    {
+        public BMI_Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void WeightBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            /// if enter key is pressed on the weight input box
+            {
+                if (WeightBox.Text.All(char.IsDigit))
+                /// if all characters inputted are numbers
+                {
+                    WeightValue.Text = WeightBox.Text;
+                    /// debugging text block (currently hidden) / also used as value for calculation
+                    WeightCheck.IsChecked = true;
+                    /// update checkbox to true
+                }
+            }
+        }
+
+        private void HeightBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (HeightBox.Text.All(char.IsDigit))
+                {
+                    HeightValue.Text = HeightBox.Text;
+                    HeightCheck.IsChecked = true;
+                }
+            }
+        }
+        /// same as above
+
+        private void WeightBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            WeightCheck.IsChecked = false;
+            WeightValue.Text = "0";
+        }
+        /// clear value and uncheck check box if text box text is changed
+
+        private void HeightBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            HeightCheck.IsChecked = false;
+            HeightValue.Text = "0";
+        }
+        /// same as above
+
+        private void CalculateButtonClick(object sender, RoutedEventArgs e)
+        {
+            bool heightCheckBool = (bool) HeightCheck.IsChecked;
+            bool weightCheckBool = (bool) WeightCheck.IsChecked;
+            /// if checkbox is checked / input is confirmed
+
+            double height = double.Parse(HeightValue.Text);
+            double weight = double.Parse(WeightValue.Text);
+            /// assign values to height and weight
+
+            if (heightCheckBool && weightCheckBool == true)
+            {
+                double BMI = Math.Round(((weight * 702) / (height * height)), 2);
+                /// math for bmi calculation
+
+                if (BMI <15)
+                {
+                    BMI_Display.Foreground = new SolidColorBrush(Colors.Blue);
+                }
+                /// if BMI is less than 15 -> change color to blue
+
+                else if (BMI > 15 && BMI < 26)
+                {
+                    BMI_Display.Foreground = new SolidColorBrush(Colors.Green);
+                }
+                /// if BMI is more than 15 and less than 26 -> change color to green
+
+                else if (BMI > 26)
+                {
+                    BMI_Display.Foreground = new SolidColorBrush(Colors.Gold);
+                }
+                /// if BMI is more than 26 -> change color to gold (it was easier to see than yellow)
+
+                BMI_Display.Text = BMI.ToString();
+                /// display BMI
+            }
+        }
+    }
+}
---
+++ Homework4_Part2/CoinTossStatCalc.cs
@@ -0,0 +1,74 @@
+﻿/// Homework No.4  Exercise No.2
+/// File Name:     CoinTossStatCalc.cs 
+/// @author:       Haydn Christian
+/// Date:          Sep. 17, 2024
+///  
+/// Problem Statement: Calculate the statistics of a certain number of coins being heads or tails 
+///     
+/// Overall Plan (step-by-step, how you want the code to make it happen): 
+/// 1. during loop, increment h/t int when inputted
+/// 2. break loop with d
+/// 3. calculate stats and diplay them if d is entered
+/// 
+/// Classes needed and Purpose coinStatCalc
+/// main class - coinStatCalc  
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Homework4_Part2
+{
+    class coinStatCalc
+    {
+        static void Main(string[] args)
+        {
+            double h, t, flips;
+            /// create doubles h (heads), t (tails), flips (total flips)   (used double instead of int so i wouldnt have to convert it later for calculating the statistics)
+            h = 0;
+            t = 0;
+            flips = 0;
+            /// assign default values
+
+            string current;
+            /// create current string to store currently inputted value later
+
+            Console.WriteLine("For each coin toss, enter h or f then enter");
+
+            while (flips < 8)
+            /// loop while there are less than 8 flips
+            {
+                current = Console.ReadLine();
+                /// assign inputted value (h/t) to current
+                if (current == "h")
+                {
+                    h++;
+                    flips++;
+                }
+                ///increment h and flips by 1
+
+                else if (current == "t")
+                {
+                    t++;
+                    flips++;
+                }
+                /// increment t and flips by 1
+
+                else
+
+                {
+                    Console.WriteLine("Plese enter either h or t");
+                }
+                /// prompt user to imput an applicable value
+
+                Console.WriteLine(h + " heads, " + t + " tails, " + flips + " total");
+                /// show current amount of heads, tails, and flips
+
+            }
+
+            Console.WriteLine(Math.Round((((h / flips) * 100)), 2) + " percent heads, " + Math.Round(((t / flips) * 100), 2) + " percent tails");
+            /// on loop completion, display statistics
+            Console.ReadLine();
+        }
+    }
+}
