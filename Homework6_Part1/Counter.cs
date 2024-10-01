f5dbda54741b7fdc012f333731d23e01bfe4a2f7
Haydn Christian <hc87006@outlook.com>
2024-09-30 19:47:54 -0700

    Homework6_Part1 upload

---
+++ Homework6_Part1/Counter.cs
@@ -0,0 +1,56 @@
+﻿/// Homework No.6  Exercise No.1
+/// File Name:     Counter.cs 
+/// @author:       Haydn Christian
+/// Date:          Sep. 30, 2024
+///  
+/// Problem Statement: Update the value of a counter using user input 
+///     
+/// Overall Plan (step-by-step, how you want the code to make it happen): 
+/// 1. Use a getter and setter to update value using user input (increment, decrement)
+/// 2. Display value on user input
+/// 3. Exit when user inputs exit command
+/// 
+/// Classes needed and Purpose - Counter (Updating values based on input), Program (Displaying output in console) 
+/// main class - Counter  
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework6_Part1
+{
+    class Counter
+    {
+        private int value;
+
+        public void setValue(int value)
+        {
+            this.value = value;
+        }
+
+        public int getValue()
+        {
+            return value;
+        }
+
+        public int Increment()
+        {
+            value++;
+            return value;
+        }
+
+        public int Decrement()
+        {
+            value--;
+            return value;
+        }
+
+        public string toString()
+        {
+            return "\nYour current value is " + value + "\n";
+        }
+    }
+
+}
---
+++ Homework6_Part1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Homework6_Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentCommand = "waiting";
+            Counter counter = new Counter();
+            counter.setValue(0);
+
+            Console.WriteLine("Menu:\nIncrement\nDecrement\nDisplay\nExit\n");
+
+            while (currentCommand == "waiting")
+            {
+                currentCommand = Console.ReadLine();
+
+                if (currentCommand == "Display")
+                {
+                    Console.WriteLine(counter.toString());
+                    currentCommand = "waiting";
+                }
+                
+                else if (currentCommand == "Increment")
+                {
+                    counter.Increment();
+                    currentCommand = "waiting";
+                }
+                
+                else if (currentCommand == "Exit")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
