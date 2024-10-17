b29c6b79e817b37433518e1ec7cdbaa0703bea61
Haydn Christian <hc87006@outlook.com>
2024-10-17 13:47:45 -0700

    Homework7_Part1 upload

---
+++ Homework7_Part1/Odometer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework7_Part1
+{
+    internal class Odometer
+    {
+        private int miles;
+        private double fuelUsed;
+
+        public Odometer()
+        {
+            
+        }
+
+        public Odometer(int _miles, double _fuelUsed)
+        {
+            Miles = _miles;
+            FuelUsed = _fuelUsed;
+        }
+
+        public int Miles { get => miles; set => miles = value; }
+        public double FuelUsed { get => fuelUsed; set => fuelUsed = value; }
+
+        public void Reset()
+        {
+            Miles = 0;
+            FuelUsed = 0;
+        }
+
+        public void Add(int m, double f)
+        {
+            Miles += m;
+            FuelUsed += f;
+        }
+        
+        public double MPG()
+        {
+            return Miles / FuelUsed;
+        }
+
+        public string ToString()
+        {
+            return $"Miles: {Miles}\nFuel Used: {FuelUsed}";
+        }
+
+    }
+}
---
+++ Homework7_Part1/Program.cs
@@ -0,0 +1,39 @@
+﻿/// Homework No. 8 Exercise No.1
+/// File Name:     program.cs 
+/// @author:       Haydn Christian
+/// Date:          Oct. 15, 2024
+///  
+/// Problem Statement: (Create an Odometer) 
+///     
+/// Overall Plan (step-by-step, how you want the code to make it happen): 
+/// 1. 
+/// 2. 
+/// 3. 
+/// etc. 
+/// 
+/// Classes needed and Purpose (Input, Processing, Output) 
+/// main class - GoodTime  
+
+using System;
+
+namespace Homework7_Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Odometer trip1 = new Odometer();
+            trip1.Add(50, 10);
+            Console.WriteLine(trip1.ToString());
+            Console.WriteLine("Fuel used for trip1: " + trip1.MPG());
+
+            Odometer trip2 = new Odometer();
+            trip2.Add(100, 20);
+            Console.WriteLine(trip2.ToString());
+            Console.WriteLine("Fuel used for trip2: " + trip2.MPG());
+
+            trip1.Reset();
+            Console.WriteLine(trip1.ToString());
+        }
+    }
+}
