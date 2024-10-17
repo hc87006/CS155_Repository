551421c30834845b92dcce5af0469fe5a477ebec
Haydn Christian <hc87006@outlook.com>
2024-10-17 16:16:05 -0700

    updated to correct formatting

--- Homework7_Lab1/Program.cs
+++ Homework7_Lab1/Program.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/// Homework No.7  Exercise No.2 
+/// File Name:     Program.cs 
+/// @author:       Haydn Christian
+/// Date:          Oct. 17 2024
+///  
+/// Problem Statement: (what you want the code to do) 
+///     
+/// Overall Plan (step-by-step, how you want the code to make it happen): 
+/// 1. Get user input for pizza (toppings, cost, size)
+/// 2. Calculate the cost of the pizza
+/// 3. Display result with ToString method
+/// etc. 
+/// 
+/// Classes needed and Purpose (Pizza, Program) 
+/// main class - Pizza
+using System;
 
 namespace Homework7_Lab1
 {
