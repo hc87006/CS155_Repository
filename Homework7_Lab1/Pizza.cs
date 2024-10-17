1ced100e3cab43d2f0e8bebc8e73865e20dc8b7d
Haydn Christian <hc87006@outlook.com>
2024-10-17 16:11:33 -0700

    Homework7_Lab1 upload

---
+++ Homework7_Lab1/Pizza.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework7_Lab1
+{
+    internal class Pizza
+    {
+        private string size;
+        private int cheese;
+        private int pepperoni;
+        private int ham;
+
+        public Pizza()
+        {
+            
+        }
+
+        public string Size { get => size; set => size = value; }
+        public int Cheese { get => cheese; set => cheese = value; }
+        public int Pepperoni { get => pepperoni; set => pepperoni = value; }
+        public int Ham { get => ham; set => ham = value; }
+
+        public Pizza(string _size, int _cheese, int _pepperoni, int _ham)
+        {
+            Size = _size;
+            Cheese = _cheese;
+            Pepperoni = _pepperoni;
+            Ham = _ham;
+        }
+
+        public double calcCost()
+        {
+            double cost = 0;
+            if (Size == "small")
+            {
+                cost = 10 + (Cheese + Pepperoni + Ham) * 2;
+            }
+            else if (Size == "medium")
+            {
+                cost = 12 + (Cheese + Pepperoni + Ham) * 2;
+            }
+            else if (Size == "large")
+            {
+                cost = 14 + (Cheese + Pepperoni + Ham) * 2;
+            }
+
+            return cost;
+        }
+
+        public string ToString()
+        {
+            return $"Size: {Size}\nCheese: {Cheese}\nPepperoni: {Pepperoni}\nHam: {Ham}\nCost: {calcCost()}";
+        }
+    }
+}
---
+++ Homework7_Lab1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Homework7_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentCommand = "waiting";
+
+            Console.WriteLine("Commands: create, exit"); 
+
+            while (currentCommand != "exit")
+            {
+                Console.WriteLine("Enter a command: ");
+                currentCommand = Console.ReadLine();
+                
+                if (currentCommand == "create")
+                {
+                    Console.WriteLine("Enter the size of the pizza: (small, medium, large)");
+                    string size = Console.ReadLine();
+                    
+                    Console.WriteLine("Enter the amount of cheese: ");
+                    int cheese = int.Parse(Console.ReadLine());
+                    
+                    Console.WriteLine("Enter the amount of pepperoni: ");
+                    int pepperoni = int.Parse(Console.ReadLine());
+                    
+                    Console.WriteLine("Enter the amount of ham: ");
+                    int ham = int.Parse(Console.ReadLine());
+                    
+                    Pizza pizza = new Pizza(size, cheese, pepperoni, ham);
+                    Console.WriteLine(pizza.ToString());
+                }
+                else if (currentCommand == "exit")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid command");
+                }
+            }
+        }
+    }
+}
