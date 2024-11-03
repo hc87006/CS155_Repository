cc25c32f68c3b1a6162c46471a38249df26906f9
Haydn Christian <hc87006@outlook.com>
2024-11-03 15:14:50 -0800

    Homework9_Lab1 upload

---
+++ Homework9_Lab1/Document.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework9_Lab1
+{
+    public class Document
+    {
+        protected string text;
+
+        public Document()
+        {
+
+        }
+
+        public Document(string text)
+        {
+            this.text = text;
+        }
+
+        public void SetText(string text)
+        {
+            this.text = text;
+        }
+
+        public override string ToString()
+        {
+            return text;
+        }
+    }
+
+    public class Email : Document
+    {
+        private string sender;
+        private string recipient;
+        private string title;
+
+        public Email(string sender, string recipient, string title, string text)
+        {
+            this.sender = sender;
+            this.recipient = recipient;
+            this.title = title;
+            this.text = text;
+        }
+
+        public void SetSender(string sender)
+        {
+            this.sender = sender;
+        }
+
+        public void SetRecipient(string recipient)
+        {
+            this.recipient = recipient;
+        }
+
+        public void SetTitle(string title)
+        {
+            this.title = title;
+        }
+
+        public string GetSender()
+        {
+            return sender;
+        }
+
+        public string GetRecipient()
+        {
+            return recipient;
+        }
+
+        public string GetTitle()
+        {
+            return title;
+        }
+
+        public override string ToString()
+        {
+            return $"From: {sender}\nTo: {recipient}\nTitle: {title}\nBody: {text}";
+        }
+    }
+
+    public class File : Document
+    {
+        private string pathname;
+
+        public File(string pathname, string text)
+        {
+            this.pathname = pathname;
+            this.text = text;
+        }
+
+        public void SetPathname(string pathname)
+        {
+            this.pathname = pathname;
+        }
+
+        public string GetPathname()
+        {
+            return pathname;
+        }
+
+        public override string ToString()
+        {
+            return $"Path: {pathname}\nContents: {text}";
+        }
+    }
+}
---
+++ Homework9_Lab1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Homework9_Lab1
+{
+    public class Program
+    {
+        public static bool ContainsKeyword(Document docObject, string keyword)
+        {
+            if (docObject.ToString().IndexOf(keyword, 0) >= 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static void Main(string[] args)
+        {
+            Email email1 = new Email("aaron@example.com", "bob@example.com", "Title1", "This is the text of Email 1.");
+            Email email2 = new Email("charlie@example.com", "dan@example.com", "Title2", "This is the text of Email 2.");
+
+            File file1 = new File("/path/to/file1", "This is the content of file 1.");
+            File file2 = new File("/path/to/file2", "This is the content of file 2.");
+
+            Console.WriteLine(ContainsKeyword(email1, "text"));
+            Console.WriteLine(ContainsKeyword(email2, "and"));
+            Console.WriteLine(ContainsKeyword(file1, "content"));
+            Console.WriteLine(ContainsKeyword(file2, "or"));
+        }
+    }
+}
