360a29b2c85e7740906a5e5d4fa495692da6b06f
Haydn Christian <hc87006@outlook.com>
2024-11-03 15:14:34 -0800

    Homework9_Part1 upload

---
+++ Homework9_Part1/Program.cs
@@ -0,0 +1,48 @@
+﻿/// Homework No. 9 Exercise No. 1
+/// File Name:     Program.cs 
+/// @author:       Haydn Christian
+/// Date:          Oct. 28, 2024
+///  
+/// Problem Statement: (what you want the code to do) 
+///     
+/// Overall Plan (step-by-step, how you want the code to make it happen): 
+/// 1. 
+/// 2. 
+/// 3. 
+/// etc. 
+/// 
+/// Classes needed and Purpose (Input, Processing, Output) 
+/// main class - GoodTime  
+
+using System;
+
+namespace Homework9_Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Aaron");
+            Person person2 = new Person("Bob");
+
+            Console.WriteLine(person1.Equals(person2));
+
+            Vehichle vehichle1 = new Vehichle();
+            vehichle1.SetENum("1234");
+            vehichle1.SetCylinders(4);
+            vehichle1.SetOwner(person1.GetName());
+            Console.WriteLine(vehichle1.ToString());
+
+            Truck truck1 = new Truck();
+            truck1.SetENum("5678");
+            truck1.SetCylinders(6);
+            truck1.SetOwner(person2.GetName());
+            truck1.SetLoadCapacity(1000);
+            truck1.SetTowCapacity(2000);
+            Console.WriteLine(truck1.ToString());
+
+            
+
+        }
+    }
+}
---
+++ Homework9_Part1/Vehichle.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework9_Part1
+{
+    public class Person
+    {
+        private string name;
+
+        public Person()
+        {
+            name = "";
+        }
+
+        public Person(string name)
+        {
+            this.name = name;
+        }
+
+        public Person(Person obj)
+        {
+            name = obj.name;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public string ToString()
+        {
+            return $"Name: {name}";
+        }
+
+        public bool Equals(Object obj)
+        {
+            return name == ((Person)obj).name;
+        }
+    }
+
+    public class Vehichle
+    {
+        protected string eNum;
+        protected int cylinders;
+        protected string owner;
+
+        public Vehichle()
+        {
+            eNum = "";
+            cylinders = 0;
+            owner = "";
+        }
+
+        public string GetENum()
+        {
+            return eNum;
+        }
+
+        public void SetENum(string eNum)
+        {
+            this.eNum = eNum;
+        }
+
+        public int GetCylinders()
+        {
+            return cylinders;
+        }
+
+        public void SetCylinders(int cylinders)
+        {
+            this.cylinders = cylinders;
+        }
+
+        public string GetOwner()
+        {
+            return owner;
+        }
+
+        public void SetOwner(string owner)
+        {
+            this.owner = owner;
+        }
+
+        public string ToString()
+        {
+            return $"eNum: {eNum}, Cylinders: {cylinders}, Owner: {owner}";
+        }
+    }
+
+    public class Truck : Vehichle
+    {
+        private double loadCapacity;
+        private int towCapacity;
+
+        public Truck()
+        {
+            loadCapacity = 0;
+            towCapacity = 0;
+        }
+
+        public Truck(string eNum, int cylinders, string owner, double loadCapacity, int towCapacity) : base()
+        {
+            this.eNum = eNum;
+            this.cylinders = cylinders;
+            this.owner = owner;
+            this.loadCapacity = loadCapacity;
+            this.towCapacity = towCapacity;
+        }
+
+        public double GetLoadCapacity()
+        {
+            return loadCapacity;
+        }
+
+        public void SetLoadCapacity(double loadCapacity)
+        {
+            this.loadCapacity = loadCapacity;
+        }
+
+        public int GetTowCapacity()
+        {
+            return towCapacity;
+        }
+
+        public void SetTowCapacity(int towCapacity)
+        {
+            this.towCapacity = towCapacity;
+        }
+
+        public string ToString()
+        {
+            return $"eNum: {eNum}, Cylinders: {cylinders}, Owner {owner}, Load Capacity: {loadCapacity}, Tow Capacity: {towCapacity}";
+        }
+    }
+}
