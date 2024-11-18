9fd71cdcb28a8abaf20d260c3c15abffa95f3fb9
Haydn Christian <hc87006@outlook.com>
2024-11-18 11:29:09 -0800

    Homework12_Part1 upload

---
+++ FinalProject/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main()
+    {
+        char[] sqr = new char[9] { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        string command = "waiting";
+
+        Console.WriteLine(command);
+
+        Console.WriteLine("Tic Tac Toe");
+
+        Player1Turn(sqr, command);
+
+    }
+
+    static void Player1Turn(char[] sqr, string command)
+    {
+        Console.WriteLine("Player 1 (X) Turn");
+        Console.WriteLine("Enter a number between 1-9");
+        DrawBoard(sqr);
+        // DoTurn(sqr, command);
+    }
+
+    static void Player2Turn(char[] sqr, string command)
+    {
+        Console.WriteLine("Player 2 (O) Turn");
+        Console.WriteLine("Enter a number between 1-9");
+        DrawBoard(sqr);
+        DoTurn(sqr, command);
+    }
+
+    public static void DoTurn(char[] sqr, string command)
+    {
+        command = "waiting";
+
+        Console.WriteLine (selectedSqr());
+
+        int selectedSqr()
+        {
+            while (command != "waiting")
+            {
+                command = Console.ReadLine();
+
+                Console.WriteLine("Line read");
+
+                if (int.Parse(command) >= 1 && int.Parse(command) <= 9)
+                {
+                    return int.Parse(command);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a number between 1-9");
+                    return 0;
+                }
+            }
+            return 0;
+        }
+    }
+
+    static void DrawBoard(char[] sqr)
+    {
+        string divider = "---+---+---";
+        Console.WriteLine($" {sqr[0]} | {sqr[1]} | {sqr[2]}\n{divider}\n {sqr[3]} | {sqr[4]} | {sqr[5]}\n{divider}\n {sqr[6]} | {sqr[7]} | {sqr[8]}\n");
+    }
+}
---
+++ Homework12_Part1/Program.cs
@@ -0,0 +1,90 @@
+﻿/// Homework No.12 Exercise No.1
+/// File Name:     Program.cs 
+/// @author:       Haydn Christian
+/// Date:          Nov. 18, 2024
+///  
+/// Problem Statement: (Use a LINQ query to determine if someone is eleigbe for the kids menu, and if someone is above average height) 
+///     
+/// Overall Plan (step-by-step, how you want the code to make it happen): 
+/// 1. Create a LINQ query and add data
+/// 2. Determine if person is under the age of 12, print
+/// 3. Determine if person is above the average height, print
+/// 
+/// Classes needed and Purpose (Program) 
+/// main class - Program 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Person> People = new List<Person>();
+        List<Person> KidsMealEligible = new List<Person>();
+        List<Person> AboveAverageHeight = new List<Person>();
+        // Initialize lists
+
+        People.Add(new Person("John", 10, 4.5));
+        People.Add(new Person("Jane", 22, 5.5));
+        People.Add(new Person("Jack", 8, 4.0));
+        People.Add(new Person("Jill", 12, 5.0));
+        People.Add(new Person("Jim", 19, 5.5));
+        // Add data
+
+        int personCount = (from p in People select p).Count();
+        double sum = 0;
+        foreach (Person person in People)
+        {
+            sum += person.Height;
+        }
+        double averageHeight = sum / personCount;
+        // Calculate average height
+
+        Console.WriteLine($"Number of people: {personCount}");
+        foreach (Person person in People)
+        {
+            Console.WriteLine($"{person.Name} (Age {person.Age}) is {person.Height} tall.");
+        }
+        // Print all people
+
+        Console.WriteLine();
+        Console.WriteLine($"Average height: {averageHeight}");
+        Console.WriteLine("Kids under 12 are eligible for the kids menu");
+        Console.WriteLine();
+        // Print basic info
+
+        KidsMealEligible.AddRange(from person in People where person.Age < 12 select person);
+        foreach (Person person in KidsMealEligible)
+        {
+            Console.WriteLine($"{person.Name} (Age {person.Age}) is eligible for the kids meal.");
+        }
+        // Add kids meal eligible people to list and print
+
+        AboveAverageHeight.AddRange(from person in People where person.Height > averageHeight select person);
+        foreach (Person person in AboveAverageHeight)
+        {
+            Console.WriteLine($"{person.Name} ({person.Height} tall) is above average height.");
+        }
+        // Add above average height people to list and print
+
+    }
+}
+
+public class Person
+{
+    string name;
+    int age;
+    double height;
+
+    public Person(string name, int age, double height)
+    {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public string Name { get => name; set => name = value; }
+    public int Age { get => age; set => age = value; }
+    public double Height { get => height; set => height = value; }
+}
